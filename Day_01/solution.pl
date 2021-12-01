71e792b8224b1fcad6bf154b13866a7b3302da47
Abigail <abigail@abigail.be>
2021-12-01 11:05:28 +0100

    Improvement for day 1

--- Day_01/solution.pl
+++ Day_01/solution.pl
@@ -9,16 +9,13 @@ no  warnings 'syntax';
 use experimental 'signatures';
 use experimental 'lexical_subs';
 
-use List::Util qw [sum];
-
 @ARGV = "input" unless @ARGV;
 
 my @depths = <>;
 my $count1 = grep {$depths [$_] > $depths [$_ - 1]} 1 .. $#depths;
-my $count2 = grep {sum (@depths [$_,     $_ - 1, $_ - 2]) >
-                   sum (@depths [$_ - 1, $_ - 2, $_ - 3])} 3 .. $#depths;
+my $count2 = grep {$depths [$_] > $depths [$_ - 3]} 3 .. $#depths;
 
 say "Solution 1: $count1";
-say "Solution 1: $count2";
+say "Solution 2: $count2";
 
 __END__
