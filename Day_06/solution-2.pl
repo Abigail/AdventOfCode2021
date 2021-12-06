4925edc121b72780058958e14e1420f13f970cf3
Abigail <adventofcode2021@abigail.freedom.nl>
2021-12-06 18:21:28 +0100

    Nitpicking

--- Day_06/solution-2.pl
+++ Day_06/solution-2.pl
@@ -19,8 +19,8 @@ my $TIMERS       =   9;
 
 my @fish = (0) x $TIMERS;
    $fish [$_] ++ foreach split /,/ => <>;
-my $fish = Math::Matrix:: -> new (map {[$_ || 0]} @fish);
 
+my $fish   = Math::Matrix:: -> new (map {[$_ || 0]} @fish);
 my $matrix = Math::Matrix:: -> new ([0, 1, 0, 0, 0, 0, 0, 0, 0],
                                     [0, 0, 1, 0, 0, 0, 0, 0, 0],
                                     [0, 0, 0, 1, 0, 0, 0, 0, 0],
@@ -31,8 +31,8 @@ my $matrix = Math::Matrix:: -> new ([0, 1, 0, 0, 0, 0, 0, 0, 0],
                                     [0, 0, 0, 0, 0, 0, 0, 0, 1],
                                     [1, 0, 0, 0, 0, 0, 0, 0, 0],);
 
-say "Solution 1: ", (($matrix ** $GENERATIONS1 * $fish) -> sum) =~ s/\.0*\s*//r;
-say "Solution 2: ", (($matrix ** $GENERATIONS2 * $fish) -> sum) =~ s/\.0*\s*//r;
+say "Solution 1: ", (($matrix ** $GENERATIONS1 * $fish) -> sum) =~ s/\..*//sr;
+say "Solution 2: ", (($matrix ** $GENERATIONS2 * $fish) -> sum) =~ s/\..*//sr;
 
 
 __END__
