ed194773d418f9c3896b0a8ee56c3264dbf6fb6a
Abigail <adventofcode2021@abigail.freedom.nl>
2021-12-13 19:20:45 +0100

    Rename some variables

--- Day_13/solution.pl
+++ Day_13/solution.pl
@@ -11,10 +11,10 @@ use experimental 'lexical_subs';
 
 @ARGV = "input" unless @ARGV;
 
-my %paper;
+my %dots;
 while (<>) {
     last unless /\S/;
-    /([0-9]+),([0-9]+)/ and $paper {$1, $2} = 1;
+    /([0-9]+),([0-9]+)/ and $dots {$1, $2} = 1;
 }
 
 my @folds = map {[/ ([xy])=([0-9]+)/]} <>;
@@ -25,10 +25,10 @@ foreach my $fold (@folds) {
     my ($dir, $coordinate) = @$fold;
     $max_x = $coordinate if $dir eq 'x';
     $max_y = $coordinate if $dir eq 'y';
-    foreach my $point (keys %paper) {
-        my ($x, $y) = split $; => $point;
+    foreach my $dot (keys %dots) {
+        my ($x, $y) = split $; => $dot;
         #
-        # Keep the point if it is above a horizonal fold,
+        # Keep the dot if it is above a horizonal fold,
         # or to the left of the a vertical fold.
         #
         next if $dir eq 'x' && $x <= $coordinate ||
@@ -37,10 +37,10 @@ foreach my $fold (@folds) {
         my $new_x = $dir eq 'x' ? 2 * $coordinate - $x : $x;
         my $new_y = $dir eq 'y' ? 2 * $coordinate - $y : $y;
         
-        $paper {$new_x, $new_y} = 1;
-        delete $paper {$point};
+        $dots {$new_x, $new_y} = 1;
+        delete $dots {$dot};
     }
-    $count1 ||= keys %paper;
+    $count1 ||= keys %dots;
 }
 
 say "Solution 1: ", $count1;
@@ -49,7 +49,7 @@ print "Solution 2: ";
 foreach my $y (0 .. $max_y - 1) {
     print "            " if $y;
     foreach my $x (0 .. $max_x - 1) {
-        print $paper {$x, $y} ? "#" : " ";
+        print $dots {$x, $y} ? "#" : " ";
     }
     print "\n";
 }
