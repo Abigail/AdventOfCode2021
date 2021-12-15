02049d740ede870bc8af62431e5e53b61179af75
Abigail <adventofcode2021@abigail.freedom.nl>
2021-12-15 13:12:33 +0100

    Day 15

--- Day_15/solution.pl
+++ Day_15/solution.pl
@@ -10,29 +10,96 @@ use experimental 'signatures';
 use experimental 'lexical_subs';
 
 @ARGV = "input" unless @ARGV;
-@ARGV = "t/input-1";
-
-my $MAX = 1 << 30;
 
 my @cave  =  map {[/[0-9]/g]} <>;
 
-my $cave1 = [map ({[@$_, $MAX]} @cave), [($MAX) x (1 + @{$cave [0]})]];
+my $SIZE_X =   @cave;
+my $SIZE_Y = @{$cave [0]};
 
-# push @cave => [($MAX) x @{$cave [0]}];
+my %seen;
+sub cell ($x, $y, $factor = 1) {
+    return if $x < 0 || $x >= $factor * $SIZE_X ||
+              $y < 0 || $y >= $factor * $SIZE_Y ||
+              $seen {$x, $y};
 
-# my $SIZE_X =   @cave      - 1;
-# my $SIZE_Y = @{$cave [0]} - 1;
+    my $val = $cave [$x % $SIZE_X] [$y % $SIZE_Y] + int ($x / $SIZE_X) +
+                                                    int ($y / $SIZE_Y);
+    $val -= 9 if $val > 9;
 
-sub solve ($cave) {
-    my $SIZE_X =   @$cave      - 1;
-    my $SIZE_Y = @{$$cave [0]} - 1;
-    my @todo   = ([0, 0, 0]);   # x/y coordinate of current point, sum of risk
+    $val;
+}
 
-    while (@todo) {
-        my ($x, $y, $risk) = @{shift @todo};
-        if ($x == $SIZE_X - 1 && $y == $SIZE_Y - 1) {
+my @heap;
+sub init_heap () {
+    @heap = ([0, 0, 0])
+}
+sub rebalance_d ($root = 0) {
+    return unless 2 * $root + 1 < @heap;
+    #
+    # Find smaller of the children.
+    #
+    my $smaller = 2 * $root + 1;
+    if (2 * $root + 2 < @heap && $heap [2 * $root + 2] [2] <
+                                 $heap [2 * $root + 1] [2]) {
+        $smaller ++;
+    }
+    #
+    # If childer are larger, we're done.
+    #
+    return if $heap [$root] [2] < $heap [$smaller] [2];
+
+    #
+    # Swap and recurse
+    #
+    @heap [$root, $smaller] = @heap [$smaller, $root];
+
+    rebalance_d ($smaller);
+}
+
+sub rebalance_u ($index = @heap - 1) {
+    #
+    # If at root, we're rebalanced.
+    #
+    return unless $index;
+
+    #
+    # If the element is larger than its parent, we're done.
+    #
+    my $parent = int (($index - 1) / 2);
+    return unless $heap [$index] [2] < $heap [$parent] [2];
+
+    #
+    # Else, swap and recurse.
+    #
+    @heap [$index, $parent] = @heap [$parent, $index];
+    rebalance_u ($parent);
+}
+
+sub extract () {
+    #
+    # Return the minimum, then rebalance.
+    #
+    my $min = $heap [0];
+    $heap [0] = pop @heap;
+    rebalance_d;
+    $min;
+}
+sub add_to_heap ($element) {
+    push @heap => $element;
+    rebalance_u;
+}
+
+
+
+sub solve ($factor = 1) {
+    %seen = ();
+    init_heap; # It's initialized with [0, 0, 0] (top-left, no-risk)
+
+    while (@heap) {
+        my ($x, $y, $risk) = @{extract ()};
+        if ($x == $SIZE_X * $factor - 1 && $y == $SIZE_Y * $factor - 1) {
             #
-            # We are at the bottom
+            # We are at the bottom-right
             #
             return $risk;
         }
@@ -40,12 +107,7 @@ sub solve ($cave) {
         #
         # We've been here, no further processing needed.
         #
-        next if $cave [$x] [$y] == $MAX;
-
-        #
-        # Mark current point as visited.
-        #
-        $cave [$x] [$y] = $MAX;
+        next if $seen {$x, $y} ++;
 
         #
         # Try all unvisited neighbours
@@ -53,21 +115,14 @@ sub solve ($cave) {
         for my $diff ([1, 0], [-1, 0], [0, 1], [0, -1]) {
             my $new_x = $x + $$diff [0];
             my $new_y = $y + $$diff [1];
-            next if $cave [$new_x] [$new_y] == $MAX;
-            push @todo => [$new_x, $new_y, $risk + $cave [$new_x] [$new_y]];
+           (my $cell  = cell ($new_x, $new_y, $factor)) // next;
+            add_to_heap [$new_x, $new_y, $risk + $cell];
         }
-
-        #
-        # Sort @todo on risk. This is linear (!)
-        #
-        @todo = sort {$$a [2] <=> $$b [2]} @todo;
     }
 }
 
-say "Solution 1: ", solve $cave1;
-
-
-
+say "Solution 1: ", solve 1;
+say "Solution 2: ", solve 5;
 
 
 
